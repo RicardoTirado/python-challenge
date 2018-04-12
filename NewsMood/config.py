a1a1125e2048b47026fa12ba8e9c7078876614a6
Ricardo Tirado <ricartirar@gmail.com>
2018-04-11 21:00:05 -0700

    added script for News Mood

---
+++ NewsMood/config.py
@@ -0,0 +1,7 @@
+consumer_key = "F4YM8k5hs0cO6bZe4xD0s0o5h"
+
+consumer_secret = "HWlRvt9aBsJBh6kEGxPgSPDaUQnlSNNKA7FIZOMLwlr4Qc5zjA"
+
+access_token = "980129278888038400-jk0QZ9FXhF4TXLwXCtm8yKNcw7KBmSL"
+
+access_token_secret = "iEgtGrnE7J5FblL9ZPrXjEifnnvxeGdNf1tJ0QzhGo4Gp"
